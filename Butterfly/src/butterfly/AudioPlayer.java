6e853b36ef02d39c732457a383174ddb528ddea5
jakewheeler <jakezro00@aim.com>
2015-10-27 21:19:49 -0400

    Removed main. New main is found in AudioPlayerUI. All testing code can go into initMain() in AudioPlayerUI. AudioControlUI is now a component of AudioPlayerUI.

--- Butterfly/src/butterfly/AudioPlayer.java
+++ Butterfly/src/butterfly/AudioPlayer.java
@@ -1,10 +1,18 @@
 package butterfly;
 
+import ui.AudioPlayerUI;
+
 /**
  *
  * @author natec
  */
 public class AudioPlayer 
 {
+    private AudioPlayerUI ui;
+    
+    public void initializeMain()
+    {
+        
+    }
     
 }
