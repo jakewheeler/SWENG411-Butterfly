d11d61328568676521b8eb8b83d305930c457ad5
Nate Christiansen <natechristiansen42@gmail.com>
2015-11-04 17:24:05 -0500

    multithreaded initMain to increase performance a bit

--- Butterfly/src/butterfly/AudioPlayer.java
+++ Butterfly/src/butterfly/AudioPlayer.java
@@ -6,6 +6,9 @@ import audio.SongList;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.Timer;
 import ui.AudioPlayerUI;
 
 /**
@@ -23,7 +26,11 @@ public final class AudioPlayer
 
     public AudioPlayer()
     {
-        initMain();
+        try {
+            initMain();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(AudioPlayer.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     public void setSearchHelper(SearchHelper helper)
@@ -41,41 +48,57 @@ public final class AudioPlayer
         this.ui = ui;
     }
     
-    public void initMain()
+    public void initMain() throws InterruptedException
     {
+        long timetodo = System.currentTimeMillis();
+        Thread getlibthread = new Thread(() -> {
+            this.manager = new LibraryManager();
+            ArrayList<File> mp3s = this.manager.getSongsInDirectory("testingsongs");
+
+            /*
+             *  add your own music directory 
+             *  watch the magic happen
+             *  still throws a ton of exceptions but we'll cross that bridge when we get to it
+             */
+            mp3s.addAll(this.manager.getSongsInDirectory("F:\\Music"));
+
+            ArrayList<Song> list = new ArrayList<>();
+
+            mp3s.stream().forEach(mp3 -> {
+                try {
+                    list.add(new Song(mp3.getPath()));
+                } catch (IOException ex) {
+                    System.out.println("Error reading " + mp3);
+                }
+            });
+            
+            this.library = new Library(list);
+        });        
+        getlibthread.start();
+        
         this.ui = new AudioPlayerUI();
         this.ui.setComponents(this);
         this.ui.setVisible(true);
-        this.manager = new LibraryManager();
-        ArrayList<File> mp3s = this.manager.getSongsInDirectory("testingsongs");
         
-        /*
-         *  add your own music directory 
-         *  watch the magic happen
-         *  still throws a ton of exceptions but we'll cross that bridge when we get to it
-         */
-        mp3s.addAll(this.manager.getSongsInDirectory("F:\\Music"));
-        
-        ArrayList<Song> list = new ArrayList<>();
-        
-        mp3s.stream().forEach(mp3 -> {
-            try {
-                list.add(new Song(mp3.getPath()));
-            } catch (IOException ex) {
-                System.out.println("Error reading " + mp3);
-            }
-        });
-                
-        this.library = new Library(list);
+        getlibthread.join();
 
-        this.audiocontrol = new AudioControl(this);
-        this.audiocontrol.setUI(this.ui.acui);
-        this.ui.acui.setController(this.audiocontrol);
+        Thread acthread = new Thread(() -> {
+            this.audiocontrol = new AudioControl(this);
+            this.audiocontrol.setUI(this.ui.acui);
+            this.ui.acui.setController(this.audiocontrol);
+        });
+        acthread.start();
         
-        this.songbrowser = new SongBrowser(this);
-        this.ui.SongBrowserUI.setController(this.songbrowser);
-        this.songbrowser.setUI(this.ui.SongBrowserUI);
-        this.songbrowser.displaySongList();
+        Thread sbthread = new Thread(() -> {
+            this.songbrowser = new SongBrowser(this);
+            this.ui.SongBrowserUI.setController(this.songbrowser);
+            this.songbrowser.setUI(this.ui.SongBrowserUI);
+            this.songbrowser.displaySongList();
+        });
+        sbthread.start();
+        acthread.join();
+        sbthread.join();
+        System.out.println(System.currentTimeMillis() - timetodo);
     }
     
     public void changeQueue(Song song, SongList newList)
